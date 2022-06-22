29102cce676773b43e8dfabc10a3688e154facc0
naoum ahmed <87044921+ahmed-naoum@users.noreply.github.com>
2022-06-22 18:33:24 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,13 +1,15 @@
 var e = document.getElementById("header");
+var E = document.getElementById("tabbir");
 window.onscroll = () => {
-    if (document.documentElement.scrollTop > 50) {
+    if (document.documentElement.scrollTop > 100) {
         e.classList.add("bacheader_blur");
         e.classList.add("border_bac");
-
-        
     } else {
-
         e.classList.remove("bacheader_blur");
         e.classList.remove("border_bac");
+    }
+    if (document.documentElement.scrollTop > 750) {
+        E.style.display = "none";
+    }else {
     }
 }
