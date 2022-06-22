7883435f671412776eae02e6974cc51496141c52
naoum ahmed <87044921+ahmed-naoum@users.noreply.github.com>
2022-06-22 18:33:57 +0100

    Create tabbirger.js

---
+++ tabbirger.js
@@ -0,0 +1,22 @@
+
+tabbir = document.getElementById("tabbir")
+var e = document.getElementById("header");
+
+
+function tpcloss(){
+    tabbir.style.display = "none";
+}
+function tap_on_tabbar(){
+    if (tabbir.style.display === "block") {
+        tabbir.style.display = "none";
+        c.
+        e.classList.remove("bacheader_blur");
+        e.classList.remove("border_bac");
+    } else {
+        tabbir.style.display = "block";
+        e.classList.add("bacheader_blur");
+        e.classList.add("border_bac");
+    }
+}
+function tap_on_closedbar(){
+}
